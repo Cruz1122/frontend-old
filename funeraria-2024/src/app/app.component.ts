4b9f1677d36c6d74d6ea40f448b44035dccaef0e
Jhonder18 <jhon.patino29550@ucaldas.edu.co>
2024-05-23 20:06:15 -0500

    Creacion de las rutas y componente de seguridad "cerrar-sesion"

--- funeraria-2024/src/app/app.component.ts
+++ funeraria-2024/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { EncabezadoComponent } from './publico/pagina-maestra/encabezado/encabezado.component';
 import { PiePaginaComponent } from './publico/pagina-maestra/pie-pagina/pie-pagina.component';
 import { MenuLateralComponent } from './publico/pagina-maestra/menu-lateral/menu-lateral.component';
@@ -9,9 +9,9 @@ import { MenuLateralComponent } from './publico/pagina-maestra/menu-lateral/menu
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, EncabezadoComponent, PiePaginaComponent, MenuLateralComponent],
+  imports: [RouterOutlet, RouterLink, EncabezadoComponent, PiePaginaComponent, MenuLateralComponent],
     templateUrl: './app.component.html',
-    styleUrl: './app.component.css'
+    styleUrls: ['./app.component.css' ]
   })
   export class AppComponent {
     title = 'funeraria-2024';
--- funeraria-2024/src/app/app.routes.ts
+++ funeraria-2024/src/app/app.routes.ts
@@ -1,3 +1,42 @@
-import { Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { InicioComponent } from "./publico/inicio/inicio.component";
+import { RutaNoEncontradaComponent } from "./publico/errores/ruta-no-encontrada/ruta-no-encontrada.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path:"inicio",
+    component: InicioComponent
+  },
+  {
+    path:"",
+    pathMatch: "full",
+    redirectTo: "/inicio"
+  },
+  {
+    path:"seguridad",
+    loadChildren: () => import("./modulos/seguridad/seguridad.module").then(mod => mod.SeguridadModule)
+  },
+  {
+    path:"parametros",
+    loadChildren: () => import("./modulos/parametros/parametros.module").then(mod => mod.ParametrosModule)
+  },
+  {
+    path:"reportes",
+    loadChildren: () => import("./modulos/reportes/reportes.module").then(mod => mod.ReportesModule)
+  },
+  {
+    path:"ventas",
+    loadChildren: () => import("./modulos/ventas/ventas.module").then(mod => mod.VentasModule)
+  },
+  {
+    path:"**",
+    component: RutaNoEncontradaComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
